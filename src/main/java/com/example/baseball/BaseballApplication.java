162156a7f5a750c69023f6a59ab8d879d3a0927d
purojyu <jun_pei74@softbank.ne.jp>
2024-07-29 17:17:09 +0900

    CORS (Cross-Origin Resource Sharing) エラーを解消

--- src/main/java/com/example/baseball/BaseballApplication.java
+++ src/main/java/com/example/baseball/BaseballApplication.java
@@ -2,10 +2,28 @@ package com.example.baseball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BaseballApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BaseballApplication.class, args);
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/baseball/api/**")
+                        .allowedOrigins("https://base-ball-3c86afa3058c.herokuapp.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
